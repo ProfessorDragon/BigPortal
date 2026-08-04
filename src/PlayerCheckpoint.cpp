a42b80ab0327675a023805ee5aef6213a777ba4d
ProfessorDragon <95201497+ProfessorDragon@users.noreply.github.com>
2026-08-04 10:29:16 -0400

    clarify usage of PlayerCheckpoint hook

--- src/PlayerCheckpoint.cpp
+++ src/PlayerCheckpoint.cpp
@@ -4,6 +4,8 @@ using namespace geode::prelude;
 
 #include <Geode/modify/PlayerCheckpoint.hpp>
 class $modify(MyPlayerCheckpoint, PlayerCheckpoint) {
+    // important!!! keep me!!!
+    // used by PlayerObject::saveToCheckpoint and loadFromCheckpoint
     struct Fields {
         float m_vehicleSize = 1.f;
     };
